3a98194c09d3fbb71d544f9b8e1550c7add0ce0e
Saiganity1 <sicatmichaeldave0411@gmail.com>
2025-09-16 14:28:08 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="green-container">
+      <div className="blue-container-top">
+        <div className="orange-box"></div>
+        <div className="red-container">
+          <div className="red-box"></div>
+          <div className="red-box"></div>
+        </div>
+        <div className="orange-box"></div> {/* New orange box */}
+      </div>
+
+      <div className="pink-container-middle">
+        <div className="large-pink-box"></div>
+        <div className="small-pink-container">
+          <div className="small-pink-box"></div>
+          <div className="small-pink-box"></div>
+
+        </div>
+      </div>
+
+      <div className="blue-container-bottom">
+        <div className="bottom-purple-box-left"></div> {/* Adjusted for new layout */}
+        <div className="bottom-orange-box-middle"></div>
+        <div className="bottom-orange-box-middle"></div>
+        <div className="bottom-purple-box-right"></div> {/* Adjusted for new layout */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
